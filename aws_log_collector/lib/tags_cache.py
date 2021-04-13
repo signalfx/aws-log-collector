59ea4e7f7ff821f76c4ac1b4d93930238ec9955b
Jarek Kawecki <jkawecki@signalfx.com>
2021-04-13 14:06:19 +0200

    Add tag support for cloudfront

--- aws_log_collector/lib/tags_cache.py
+++ aws_log_collector/lib/tags_cache.py
@@ -1,13 +1,18 @@
 import boto3
+from botocore.config import Config
 import time
 from aws_log_collector.logger import log
 
 SUPPORTED_NAMESPACES = ["lambda", "rds", "eks", "apigateway", "s3", "elasticloadbalancing",
-                        "redshift", "cloudfront"]
+                        "redshift"]
+
+SUPPORTED_GLOBAL_NAMESPACES = ["cloudfront"]
 
 
 class TagsCache(object):
     resource_tagging_client = boto3.client("resourcegroupstaggingapi")
+    global_resource_tagging_client = boto3.client("resourcegroupstaggingapi",
+                                                  config=Config(region_name="us-east-1"))
 
     def __init__(self, cache_ttl_seconds):
         self.tags_by_arn = {}
@@ -30,18 +35,25 @@ class TagsCache(object):
 
     def _build_cache(self):
         tags_by_arn_cache = {}
-        get_resources_paginator = self.resource_tagging_client.get_paginator("get_resources")
+
+        TagsCache._load_tags(self.resource_tagging_client, SUPPORTED_NAMESPACES, tags_by_arn_cache)
+        TagsCache._load_tags(self.global_resource_tagging_client, SUPPORTED_GLOBAL_NAMESPACES, tags_by_arn_cache)
+
+        return tags_by_arn_cache
+
+    @staticmethod
+    def _load_tags(resource_tagging_client, supported_namespaces, tags_by_arn_cache):
+        get_resources_paginator = resource_tagging_client.get_paginator("get_resources")
 
         try:
             for page in get_resources_paginator.paginate(
-                    ResourceTypeFilters=SUPPORTED_NAMESPACES, ResourcesPerPage=100
+                    ResourceTypeFilters=supported_namespaces, ResourcesPerPage=100
             ):
-                page_tags_by_arn = self._parse_get_resources_response_for_tags_by_arn(page)
+                page_tags_by_arn = TagsCache._parse_get_resources_response_for_tags_by_arn(page)
                 tags_by_arn_cache.update(page_tags_by_arn)
         except Exception as ex:
             log.exception(f"Encountered an Exception when trying to fetch tags (exception={ex})")
 
-        return tags_by_arn_cache
 
     @staticmethod
     def _parse_get_resources_response_for_tags_by_arn(resources_page):
