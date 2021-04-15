b37febdc9602964dd8f9ec161c56409ae36e119e
Jarek Kawecki <52408797+jkawecki-signalfx@users.noreply.github.com>
2021-04-15 09:03:02 +0200

    Add tag support for cloudfront, load balancers and redshift (#31)
    * Add tag support for load balancers and redshift
    
    * Add tag support for cloudfront
    
    * Skip fetching global namespaces for tags if GOV or CHINA
    
    * Updates after a review

--- aws_log_collector/lib/tags_cache.py
+++ aws_log_collector/lib/tags_cache.py
@@ -1,13 +1,33 @@
 import boto3
+from botocore.config import Config
 import time
-
 from aws_log_collector.logger import log
 
-SUPPORTED_NAMESPACES = ["lambda", "rds", "eks", "apigateway", "s3"]
+SUPPORTED_NAMESPACES = ["lambda", "rds", "eks", "apigateway", "s3", "elasticloadbalancing",
+                        "redshift", "cloudfront"]
+
+# tests performed show that tags for the cloudfront namespace may not be available in the region
+# where the log collector is installed, actually the tags were present in us-east-1 region and
+# that's why the SUPPORTED_GLOBAL_NAMESPACES was introduced
+#
+# note that the cloudfront is also added to the SUPPORTED_NAMESPACES... it is because I couldn't
+# find any official confirmation that observations coming from tests are valid
+SUPPORTED_GLOBAL_NAMESPACES = ["cloudfront"]
 
 
 class TagsCache(object):
-    resource_tagging_client = boto3.client("resourcegroupstaggingapi")
+    session = boto3.session.Session()
+
+    resource_tagging_client = session.client("resourcegroupstaggingapi")
+
+    regions = session.get_available_regions("resourcegroupstaggingapi", partition_name="aws")
+    log.debug(f"available regions: {regions}")
+
+    global_region_available = "us-east-1" in regions
+
+    if global_region_available:
+        global_resource_tagging_client = boto3.client("resourcegroupstaggingapi",
+                                                      config=Config(region_name="us-east-1"))
 
     def __init__(self, cache_ttl_seconds):
         self.tags_by_arn = {}
@@ -30,18 +50,27 @@ class TagsCache(object):
 
     def _build_cache(self):
         tags_by_arn_cache = {}
-        get_resources_paginator = self.resource_tagging_client.get_paginator("get_resources")
+
+        TagsCache._load_tags(self.resource_tagging_client, SUPPORTED_NAMESPACES, tags_by_arn_cache)
+        if self.global_region_available:
+            TagsCache._load_tags(self.global_resource_tagging_client,
+                                 SUPPORTED_GLOBAL_NAMESPACES, tags_by_arn_cache)
+
+        return tags_by_arn_cache
+
+    @staticmethod
+    def _load_tags(resource_tagging_client, supported_namespaces, tags_by_arn_cache):
+        get_resources_paginator = resource_tagging_client.get_paginator("get_resources")
 
         try:
             for page in get_resources_paginator.paginate(
-                    ResourceTypeFilters=SUPPORTED_NAMESPACES, ResourcesPerPage=100
+                    ResourceTypeFilters=supported_namespaces, ResourcesPerPage=100
             ):
-                page_tags_by_arn = self._parse_get_resources_response_for_tags_by_arn(page)
+                page_tags_by_arn = TagsCache._parse_get_resources_response_for_tags_by_arn(page)
                 tags_by_arn_cache.update(page_tags_by_arn)
         except Exception as ex:
             log.exception(f"Encountered an Exception when trying to fetch tags (exception={ex})")
 
-        return tags_by_arn_cache
 
     @staticmethod
     def _parse_get_resources_response_for_tags_by_arn(resources_page):
@@ -50,6 +79,7 @@ class TagsCache(object):
         aws_resource_list = resources_page["ResourceTagMappingList"]
         for aws_resource in aws_resource_list:
             arn = aws_resource["ResourceARN"].lower()
+            log.debug(f"loading tags for {arn}")
             aws_tags = aws_resource["Tags"]
             tags = tags_by_arn.get(arn, {})
             for raw_tag in aws_tags:
