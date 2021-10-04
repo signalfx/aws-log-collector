c44167fe5e30e4ceaba7b762fefaecef25d51498
Viktor Taranenko <63248085+vtaranenko@users.noreply.github.com>
2021-10-04 17:04:25 +0200

    Restored the test which was removed by mistake (#44)

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -82,6 +82,18 @@ class LogCollectingSuite(TestCase):
 
         send_method_mock.assert_called_with([json.dumps(expected_event)])
 
+    def test_s3_s3(self, tags_cache_get_mock, send_method_mock, s3_service_read_lines_mock, _):
+        scenario = {
+            "name": "s3",
+            "arn_to_tags": {
+                "arn:aws:s3:::integrations-team":
+                    {"bucket-tag-1": 1, "bucket-tag-2": "abc"},
+                "arn:aws:s3:::integrations-team/WhatsApp Image 2020-07-05 at 18.34.43.jpeg":
+                    {"object-tag-1": 10, "object-tag-2": "def"}
+            }
+        }
+        self._test_s3_logs_handling(tags_cache_get_mock, send_method_mock, s3_service_read_lines_mock, scenario)
+
     def test_s3_invalid_s3(self, tags_cache_get_mock, send_method_mock, s3_service_read_lines_mock, _):
         scenario = {
             "name": "invalid_s3",
